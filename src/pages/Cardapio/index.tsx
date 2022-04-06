5fbcb852dff93915fc14d6aa827c85f8b94f79bf
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-04-06 10:18:54 -0300

    Adicionando assets

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,12 @@
-import styles from './Cardapio.module.scss'
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from 'assets/logo.svg';
 
 export default function Cardapio() {
-    return (
-        <main>
-            <nav className={styles.menu}>
-            
-            </nav>
-        </main>
-    );
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+    </main>
+  )
 }
